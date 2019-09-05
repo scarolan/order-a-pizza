2057274ecdd9738d516ffe2f032196bd67f30e62
Sean Carolan <scarolan@hashicorp.com>
2019-09-05 14:15:56 -0500

    try again

--- terraform/main.tf
+++ terraform/main.tf
@@ -127,12 +127,12 @@ data "dominos_menu_item" "drink1" {
 
 resource "dominos_order" "order" {
   address_api_object = data.dominos_address.addr.api_object
-  item_codes         = tolist([
+  item_codes         = tolist(
     data.dominos_menu_item.pizza1[*].matches[0].code,
     data.dominos_menu_item.pizza1[*].matches[0].code,
     data.dominos_menu_item.pizza1[*].matches[0].code
     #"P14IREUH", "P14IREUH", "P14IREUH" 
-  ])
+  )
   store_id           = data.dominos_store.store.store_id
 }
 
