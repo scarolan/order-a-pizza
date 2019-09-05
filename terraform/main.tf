b74fc8011d8ee147ed48b892120b0a97b5c64bf0
Sean Carolan <scarolan@hashicorp.com>
2019-09-05 14:26:16 -0500

    Try something

--- terraform/main.tf
+++ terraform/main.tf
@@ -127,9 +127,10 @@ data "dominos_menu_item" "drink1" {
 
 resource "dominos_order" "order" {
   address_api_object = data.dominos_address.addr.api_object
-  item_codes         = concat(
-    data.dominos_menu_item.pizza1[*].matches[0].code,
-    data.dominos_menu_item.pizza1[*].matches[0].code)
+  item_codes         = [
+    data.dominos_menu_item.pizza1[*].matches[0].code[0],
+    data.dominos_menu_item.pizza1[*].matches[0].code[0]
+  ]
   store_id           = data.dominos_store.store.store_id
 }
 
