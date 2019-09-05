41bbbbd9f3433cc6b93501e0beb20fac2b381ba3
Sean Carolan <scarolan@hashicorp.com>
2019-09-05 15:00:16 -0500

    Disable outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,24 +142,24 @@ output "locals" {
   value = local.pizza1_list
 }
 
-output "pizza1" {
-  value = [
-    for pizza in data.dominos_menu_item.pizza1:
-      {
-        name = pizza.matches[0].name
-        code = pizza.matches[0].code
-        price_cents = pizza.matches[0].price_cents
-      }
-  ]
-}
+# output "pizza1" {
+#   value = [
+#     for pizza in data.dominos_menu_item.pizza1:
+#       {
+#         name = pizza.matches[0].name
+#         code = pizza.matches[0].code
+#         price_cents = pizza.matches[0].price_cents
+#       }
+#   ]
+# }
 
-output "drinks" {
-  value = [
-    for drink in data.dominos_menu_item.drink1:
-      {
-        name = drink.matches[0].name
-        code = drink.matches[0].code
-        price_cents = drink.matches[0].price_cents
-      }
-  ]
-}
+# output "drinks" {
+#   value = [
+#     for drink in data.dominos_menu_item.drink1:
+#       {
+#         name = drink.matches[0].name
+#         code = drink.matches[0].code
+#         price_cents = drink.matches[0].price_cents
+#       }
+#   ]
+# }
